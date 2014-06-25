26b63ee6fcc38fb3bc9b4ff91cc39bfccac2dc61
Steve Kim <ElSteve05@gmail.com>
2014-06-24 23:08:44 -0500

    Added code for caching searches. Code is still untested

--- labs/decorator/InstrumentedAirportScraper.java
+++ labs/decorator/InstrumentedAirportScraper.java
@@ -1,4 +1,25 @@
+import java.util.HashMap;
+import java.util.List;
+
 public class InstrumentedAirportScraper implements AirportScraperInterface {
+    AirportScraper airportScraper;
+    HashMap<String, Double> searchCache = new HashMap<String,Double>();
+    
+
+    public InstrumentedAirportScraper(AirportScraper airportScraper) {
+        this.airportScraper = airportScraper;
+    }
+
+    @Override
+    public double lookupDistance(String dest) {
+        if (searchCache.containsKey(dest)) {
+            return searchCache.get(dest);
+        } else {
+            double distance = airportScraper.lookupDistance(dest);
+            searchCache.put(dest, distance);
+            return distance;
+        }
+    }
 
     // sorted by most commonly called destinations first,
     // ties broken alphabetically. ok to consider case, i.e.,
