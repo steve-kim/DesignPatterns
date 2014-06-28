c2b111bafc89dde427548b80571d278ef6e3506d
Steve Kim <ElSteve05@gmail.com>
2014-06-27 22:22:42 -0500

    Finished adding in initial code. Starting to test

--- labs/decorator/InstrumentedAirportScraper.java
+++ labs/decorator/InstrumentedAirportScraper.java
@@ -1,10 +1,10 @@
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
-public class InstrumentedAirportScraper implements AirportScraperInterface {
+public class InstrumentedAirportScraper implements AirportScraperInterface
+{
     AirportScraper airportScraper;
     HashMap<String, Double> searchCache = new HashMap<String,Double>();
-    
+    HashMap<String, Integer> searchFrequency = new HashMap<String, Integer>();
 
     public InstrumentedAirportScraper(AirportScraper airportScraper) {
         this.airportScraper = airportScraper;
@@ -13,10 +13,13 @@ public class InstrumentedAirportScraper implements AirportScraperInterface {
     @Override
     public double lookupDistance(String dest) {
         if (searchCache.containsKey(dest)) {
+            int i = searchFrequency.get(dest) + 1;
+            searchFrequency.put(dest, i);
             return searchCache.get(dest);
         } else {
             double distance = airportScraper.lookupDistance(dest);
             searchCache.put(dest, distance);
+            searchFrequency.put(dest, 1);
             return distance;
         }
     }
@@ -26,6 +29,38 @@ public class InstrumentedAirportScraper implements AirportScraperInterface {
     // treat AUS and aus differently.  see test suite
     // for exact format
     public List<String> mostCommonDestinations() {
+        ValueComparator sorter = new ValueComparator(searchFrequency);
+        TreeMap<String, Integer> sorted_map = new TreeMap<String, Integer>(sorter);
+        ArrayList<String> results = new ArrayList<String>();
+
+        //This sorts the HashMap
+        sorted_map.putAll(searchFrequency);
+        HashSet<String> sorted_keys = (HashSet)sorted_map.keySet();
+        Iterator<String> iterator = sorted_keys.iterator();
+
+        while (iterator.hasNext()) {
+            results.add(iterator.next());
+        }
+
+        return results;
+    }
+
+    class ValueComparator implements Comparator<String> {
+        Map<String, Integer> base;
+
+        public ValueComparator(Map<String, Integer> base) {
+            this.base = base;
+        }
+
+        @Override
+        public int compare(String a, String b) {
+            if (base.get(a) < base.get(b))
+                return -1;
+            else if (base.get(a) == base.get(b))
+                return a.compareTo(b);
+            else
+                return 1;
+        }
     }
 
 }
